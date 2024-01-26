4a1c9ca0439976a0dc403606ef0a51584e06afc3
Luis Fer <lfern5113@gmail.com>
2024-01-26 13:35:25 -0600

    Clase usuario creado

---
+++ src/Usuario.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lfern
+ */
+public class Usuario {
+    String nombre_usuario;
+    String contra;
+    String tipo_usuario;
+
+    public Usuario(String nombre_usuario, String contra, String tipo_usuario) {
+        this.nombre_usuario = nombre_usuario;
+        this.contra = contra;
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre_usuario=" + nombre_usuario + ", contra=" + contra + ", tipo_usuario=" + tipo_usuario + '}';
+    }
+    
+    
+}
--- src/lab2p2_luiscastro/Lab2P2_LuisCastro.java
+++ src/lab2p2_luiscastro/Lab2P2_LuisCastro.java
@@ -4,17 +4,48 @@
  */
 package lab2p2_luiscastro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lfern
  */
 public class Lab2P2_LuisCastro {
-
+static Scanner leer = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        System.out.println("*****Menu*****");
+        System.out.println("1. Listar Recursos.");
+        System.out.println("2. Crear Recursos. (Profesores y Bibliotecarios)");
+        System.out.println("3. Eliminar Recursos. (Bibliotecarios)");
+        System.out.println("4. Modificar Recursos.");
+        System.out.println("5. Salir.");
+        System.out.println("Ingrese su numero: ");
+        int opcion = leer.nextInt();
+        while(opcion !=5){
+            switch(opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }//FIN DEL SWITCH
+            System.out.println("*****Menu*****");
+            System.out.println("1. Listar Recursos.");
+            System.out.println("2. Crear Recursos. (Profesores y Bibliotecarios)");
+            System.out.println("3. Eliminar Recursos. (Bibliotecarios)");
+            System.out.println("4. Modificar Recursos.");
+            System.out.println("5. Salir.");
+            System.out.println("Ingrese su numero: ");
+            opcion = leer.nextInt();
+        }//FIN DEL WHILE
     }
     
 }
